8452a0f4a5fb0598366cc65662aac2c1e85fa584
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-19 17:56:59 +0200

    added jumbo with button to see popular films and tv series

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,11 +1,15 @@
 <template>
     <main>
         <div class="alert alert-danger" v-if="store.error.message">{{ store.error.message }}</div>
-        <div v-if="!store.loading">
-            <CardList v-if="store.options.params.query == ''" title="Film Popolari" :list="store.popularMovie" />
-            <CardList v-if="store.options.params.query == ''" title="Serie Tv Popolari" :list="store.popularTv" />
-            <CardList title="Movies" :list="store.movieList" v-if="store.options.params.query != ''" />
-            <CardList title="Tv Series" :list="store.tvList" v-if="store.options.params.query != ''" />
+        <div class="hero" v-if="store.options.params.query == ''">
+            <video src="/images/Creed3-Trailer.mp4" autoplay muted loop></video>
+        </div>
+        <a class="button-popular" href="#popular"><i class="fa-solid fa-arrow-down"></i></a>
+        <div id="popular" class="main" v-if="!store.loading">
+            <CardList class="ldg-margin" v-if="store.options.params.query == ''" title="Film Popolari" :list="store.popularMovie" />
+            <CardList class="ldg-margin" v-if="store.options.params.query == ''" title="Serie Tv Popolari" :list="store.popularTv" />
+            <CardList class="ldg-margin" title="Movies" :list="store.movieList" v-if="store.options.params.query != ''" />
+            <CardList class="ldg-margin" title="Tv Series" :list="store.tvList" v-if="store.options.params.query != ''" />
         </div>
         <div v-else>
             <ApiLoader />
@@ -35,5 +39,25 @@
     @use '../assets/styles/partials/variables.scss' as *;
     main{
         padding-top: 90px;
+    }
+
+    .ldg-margin{
+        margin-top: 40px;
+    }
+
+    .button-popular{
+        position: absolute;
+        top: 800px;
+        z-index: 1000;
+        right: 20px;
+        text-decoration: none;
+        font-size: 50px;
+        color: $red;
+    }
+
+    video{
+        position: relative;
+        margin-top: -200px;
+        width: 100%;
     }
 </style>
